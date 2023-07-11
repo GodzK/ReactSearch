855f99f891058411728c04a0dd5d90415d12cf6e
GodzK <64467457+GodzK@users.noreply.github.com>
2023-07-11 21:17:30 +0700

    zinglecode

---
+++ myapp/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className ="app">
+     <header className = "app-header">
+      Phakaphol Picture Albumn JSX
+     </header>
+     <div className="app-grid">
+      <div className='pictureitem'>
+       <img src="pk2.jpg" ></img>
+       <h4>beckham</h4>
+      </div>
+      <div className='pictureitem'>
+       <img src="pk3.jpg" ></img>
+       <h4>beckham</h4>
+      </div>
+      <div className='pictureitem'>
+       <img src="pk4.jpg" ></img>
+       <h4>beckham</h4>
+      </div>
+      <div className='pictureitem'>
+       <img src="pk5.png" ></img>
+       <h4>beckham</h4>
+      </div>
+      
+      
+
+     </div>
+    </div>
+
+  );
+}
+
+export default App;
