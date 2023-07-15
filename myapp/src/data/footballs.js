4b2052fcbf2536c4378b77ab039939b9ca497a83
GodzK <64467457+GodzK@users.noreply.github.com>
2023-07-15 23:11:56 +0700

    add a lot of comcamp picture (ep1)

--- myapp/src/data/footballs.js
+++ myapp/src/data/footballs.js
@@ -1,24 +1,105 @@
 const footballs = [
     {
-      title : 'ค่าย',
-      thumbnailUrl :"cs.png"
-      
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc01.jpg",
     },
     {
-      title : "ค่าย",
-      thumbnailUrl :"kaset.jpg"
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc1.JPG",
     },
     {
-        title : "เเข่ง",
-        thumbnailUrl :"compet.jpg"
-      },
-      {
-        title : "ประจำวัน",
-        thumbnailUrl :"daily.jpg"
-      },
-      {
-        title : "ประจำวัน",
-        thumbnailUrl :"central.png"
-      },
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc3.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc4.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc5.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc6.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc7.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc8.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc9.jpeg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc10.jpeg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc11.jpeg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc12.jpeg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc13.jpg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc14.jpg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc15.jpeg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc16.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc17.JPG",
+    },   {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc18.jpg",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc19.JPG",
+    },
+
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc20.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc21.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc22.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc23.JPG",
+    },
+    {
+      title : 'comcamp',
+      thumbnailUrl :"./picture/comcamp/cc24.JPG",
+    },
+     {
+      title : 'cs',
+      thumbnailUrl :"./picture/comcamp/cc24.JPG",
+    },
+
+    
   ];
   export default footballs;
